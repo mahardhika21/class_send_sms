de32e332550fa55f2093ae46dc054128848cefa0
mahardhika <mahardhika894@gmail.com>
2019-04-23 09:50:40 +0700

    add library

---
+++ ZenzivaLib.php
@@ -0,0 +1,42 @@
+<?php
+
+class ZenzivaLib {
+	private $user_key;
+	private $pass_key;
+	private $url;
+	private $ci;
+	
+	function __construct(){
+		$this->ci =& get_instance();
+		$this->user_key = $this->ci->config->item('user_key', 'sms');
+		$this->pass_key = $this->ci->config->item('pass_key', 'sms');
+		$this->url = $this->ci->config->item('url', 'sms');
+	}
+	
+	function send_sms($telepon, $message){
+		// $userkey = "xxxxxx";
+		// $passkey = "xxxxxx";
+		// $telepon = "081234567890";
+		// $message = "Terima Kasih, pendaftaran atas nama $nama telah.";
+		// $url = "https://reguler.zenziva.net/apps/smsapi.php";
+		$curlHandle = curl_init();
+		curl_setopt($curlHandle, CURLOPT_URL, $this->url);
+		curl_setopt($curlHandle, CURLOPT_POSTFIELDS,
+		'userkey='.$this->user_key.'&passkey='.$this->pass_key.'&nohp='.$telepon.'&pesan='.urlencode($message));
+		curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($curlHandle, CURLOPT_TIMEOUT,30);
+		curl_setopt($curlHandle, CURLOPT_POST, 1);
+		$results = curl_exec($curlHandle);
+		curl_close($curlHandle);
+		// $XMLdata = new SimpleXMLElement($results);
+		$xml = simplexml_load_string($results);
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+		// $status = $XMLdata->message[0]->text;
+		return $array;
+	
+	}
+}
